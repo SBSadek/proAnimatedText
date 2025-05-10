1f24728c5d8f2393db8d4f50802bfeb7e60768da
sbsadekali <developersadekali@gmail.com>
2025-05-11 00:38:05 +0600

    Initial commit git tag v1.0 git push origin v1.0

--- app/src/main/java/com/sbsoftwareltd/proanimatedtext/MainActivity.java
+++ app/src/main/java/com/sbsoftwareltd/proanimatedtext/MainActivity.java
@@ -8,7 +8,12 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.sbsoftwareltd.animatedtextpro.Animatedtextpro;
+
 public class MainActivity extends AppCompatActivity {
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +25,22 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+
+        //========
+
+        Animatedtextpro typingTextView = findViewById(R.id.typingTextView);
+        typingTextView.setCharacterDelay(100); // প্রতিটি অক্ষরের জন্য বিলম্ব
+        typingTextView.setTipDelay(2000); // প্রতিটি টিপসের মাঝে বিলম্ব
+
+        String[] tips = {
+                "Welcome to our app!",
+                "Enjoy fast performance.",
+                "Built with ❤️ by SB Software."
+        };
+
+        typingTextView.setTips(tips);
+
+        //===
     }
 }
